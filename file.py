f01fe2e1bacc09ed884c5f20bccad70bb202a472
Giulia <giulia@pepes-MacBook-Pro.local>
2020-01-23 15:10:55 +0100

    create new file2.py and add comment to file.py

--- file.py
+++ file.py
@@ -6,5 +6,6 @@ Created on Thu Jan 23 14:26:09 2020
 @author: giulia
 """
 
+# this function sums two values
 def func(a, b):
     return a + b
---
+++ file2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 23 15:07:57 2020
+
+@author: giulia
+"""
+
